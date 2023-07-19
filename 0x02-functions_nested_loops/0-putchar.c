baa9a9541bfdb3cccb3915527a4f203ebf1307af
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-07-19 03:14:59 +0200

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/* more headers here */
+
+/* documentation of function here */
+int main(void)
+{
+	char *str = "_putchar\n";
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+
+	return 0;
+}
