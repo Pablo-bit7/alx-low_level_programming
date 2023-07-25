72580173eaf049a99b08b993a8605cb276713cb2
Paballo Mogane <122962125+Pablo-bit7@users.noreply.github.com>
2023-07-25 03:09:38 +0000

    Fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,13 +8,13 @@ void rev_string(char *s) {
     int i, j;
     char temp;
 
-    // Calculate the length of the string
-    while (s[length] != '\0') {
+    while (s[length] != '\0')
+    {
         length++;
     }
 
-    // Swap characters to reverse the string
-    for (i = 0, j = length - 1; i < j; i++, j--) {
+    for (i = 0, j = length - 1; i < j; i++, j--)
+    {
         temp = s[i];
         s[i] = s[j];
         s[j] = temp;
