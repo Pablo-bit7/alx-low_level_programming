e97412ebf15032c89dd380db509ce120035bd796
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-07-26 04:52:53 +0200

    betty

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
 /**
-* prints a string, followed by a new line, to stdout
+* _puts - prints a string, followed by a new line, to stdout
+*
+* @str: holds string array
 */
-void _puts(char *str) {
-    while (*str) {
-        _putchar(*str);
-        str++;
-    }
-    _putchar('\n');
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
 }
