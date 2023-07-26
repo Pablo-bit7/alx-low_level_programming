1990c59d7b56e1a21eeb6f732d1557ea61a7954d
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-07-26 03:32:06 +0200

    Betty

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,12 @@
 #include "main.h"
 
 /**
-* takes `*n` as parameter and updates the value it points to to 98
+* reset_to_98 - takes `*n` as parameter and updates
+* the value it points to to 98
+*
+* @n: dereferences to int parameter 98
 */
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
