be220a14299e85466fc45d8ebb815e2272793f12
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-07-26 05:11:48 +0200

    betty

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,28 +1,34 @@
 #include "main.h"
 
 /**
-* prints second half of a string, followed by a new line
+* puts_half - prints second half of a string, followed by a new line.
+* If number of characters is odd, prints the last n characters
+* of the string, where n = (length_of_the_string - 1) / 2
 *
-* If number of characters is odd, prints the last n characters of the string, where n = (length_of_the_string - 1) / 2
+* @str: holds string array
 */
 void puts_half(char *str)
 {
-    int length = 0;
-    int i;
-    int start_index;
+	int length = 0;
+	int i;
+	int start_index;
 
-    while (str[length] != '\0') {
-        length++;
-    }
+	while (str[length] != '\0')
+	{
+		length++;
+	}
 
-    start_index = length / 2;
-    if (length % 2 != 0) {
-        start_index = (length + 1) / 2;
-    }
+	start_index = length / 2;
 
-    for (i = start_index; i < length; i++) {
-        _putchar(str[i]);
-    }
+	if (length % 2 != 0)
+	{
+		start_index = (length + 1) / 2;
+	}
 
-    _putchar('\n');
+	for (i = start_index; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
 }
