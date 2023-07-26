f37805b144d9f725b0c8fcc73ee167f3c26ed29c
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-07-26 04:58:09 +0200

    betty

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,24 @@
 #include "main.h"
 
 /**
-* prints a string, in reverse, followed by a new line
+* print_rev - prints a string, in reverse, followed by a new line
+*
+* @s: holds string array
 */
 void print_rev(char *s)
 {
-    int length = 0;
-    int i;
+	int length = 0;
+	int i;
 
-    while (s[length] != '\0')
-    {
-        length++;
-    }
+	while (s[length] != '\0')
+	{
+		length++;
+	}
 
-    for (i = length - 1; i >= 0; i--)
-    {
-        _putchar(s[i]);
-    }
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
