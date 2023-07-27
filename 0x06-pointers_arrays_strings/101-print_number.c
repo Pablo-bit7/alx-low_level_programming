adeb22888ad940e58b38136fb0396e951aae8091
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-07-27 04:52:32 +0200

    betty

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,32 +7,32 @@
  */
 void print_number(int n)
 {
-    int reverse = 0;
-    int digit;
+	int reverse = 0;
+	int digit;
 
-    if (n == 0)
-    {
-        _putchar('0');
-        return;
-    }
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 
-    if (n < 0)
-    {
-        _putchar('-');
-        n = -n;
-    }
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
 
-    while (n > 0)
-    {
-        digit = n % 10;
-        reverse = reverse * 10 + digit;
-        n /= 10;
-    }
+	while (n > 0)
+	{
+		digit = n % 10;
+		reverse = reverse * 10 + digit;
+		n /= 10;
+	}
 
-    while (reverse > 0)
-    {
-        digit = reverse % 10;
-        _putchar('0' + digit);
-        reverse /= 10;
-    }
+	while (reverse > 0)
+	{
+		digit = reverse % 10;
+		_putchar('0' + digit);
+		reverse /= 10;
+	}
 }
