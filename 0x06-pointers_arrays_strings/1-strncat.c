677b3f7bf1c9904d636480c9000b263b2067510b
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-07-27 02:08:55 +0200

    betty

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,28 +10,28 @@
 * @n: number of bytes from src
 *
 * Return: dest
-**/
+*/
 char *_strncat(char *dest, char *src, int n)
 {
-    char *ptr = dest;
+	char *ptr = dest;
 
-    /* Move the pointer to the end of the destination string */
-    while (*ptr)
-    {
-        ptr++;
-    }
+	/* Move the pointer to the end of the destination string */
+	while (*ptr)
+	{
+		ptr++;
+	}
 
-    /* Append the src string to the destination string, up to n characters */
-    while (*src && n > 0)
-    {
-        *ptr = *src;
-        ptr++;
-        src++;
-        n--;
-    }
+	/* Append the src string to the destination string, up to n characters */
+	while (*src && n > 0)
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+		n--;
+	}
 
-    /* Add the terminating null byte */
-    *ptr = '\0';
+	/* Add the terminating null byte */
+	*ptr = '\0';
 
-    return (dest);
+	return (dest);
 }
