8956f18ec5b39a0c229d36e630ded34411ec8a3c
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-07-27 04:16:06 +0200

    betty

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,16 +9,16 @@
  */
 char *string_toupper(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i])
-    {
-        if (str[i] >= 'a' && str[i] <= 'z')
-        {
-            str[i] = str[i] - 'a' + 'A';
-        }
-        i++;
-    }
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 'a' + 'A';
+		}
+		i++;
+	}
 
-    return (str);
+	return (str);
 }
