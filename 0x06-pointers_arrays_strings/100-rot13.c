3319c0c34a9cff797d105ead83875ceeff66b96a
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-07-27 04:45:11 +0200

    betty

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,27 +2,28 @@
 
 /**
  * rot13 - Encodes a string using rot13.
+ *
  * @str: The string to encode.
  *
  * Return: A pointer to the resulting encoded string.
  */
 char *rot13(char *str)
 {
-    char *input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    char *output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-    int i, j;
+	char *input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
 
-    for (i = 0; str[i]; i++)
-    {
-        for (j = 0; input[j]; j++)
-        {
-            if (str[i] == input[j])
-            {
-                str[i] = output[j];
-                break;
-            }
-        }
-    }
+	for (i = 0; str[i]; i++)
+	{
+		for (j = 0; input[j]; j++)
+		{
+			if (str[i] == input[j])
+			{
+				str[i] = output[j];
+				break;
+			}
+		}
+	}
 
-    return (str);
+	return (str);
 }
