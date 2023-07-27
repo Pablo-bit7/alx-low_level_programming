5939b7b1296f630e74889c407588e2dece8accdc
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-07-27 04:08:27 +0200

    betty

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -5,15 +5,17 @@
 *
 * @a: holds array of integers
 *
-* @n: number of elements
+* @n: number of elements to swap
 */
-void reverse_array(int *a, int n) {
-    int temp;
-    int i;
+void reverse_array(int *a, int n)
+{
+	int temp;
+	int i;
 
-    for (i = 0; i < n / 2; i++) {
-        temp = a[i];
-        a[i] = a[n - 1 - i];
-        a[n - 1 - i] = temp;
-    }
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+	}
 }
