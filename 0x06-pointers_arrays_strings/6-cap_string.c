aa50586ace1d065ade5ab45af876fbc40858280c
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-07-27 04:28:40 +0200

    betty

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,23 +9,23 @@
  */
 char *cap_string(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i])
-    {
-        if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n'
-            || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.'
-            || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '\"'
-            || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{'
-            || str[i - 1] == '}')
-        {
-            if (str[i] >= 'a' && str[i] <= 'z')
-            {
-                str[i] = str[i] - 'a' + 'A';
-            }
-        }
-        i++;
-    }
+	while (str[i])
+	{
+		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n'
+		|| str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.'
+		|| str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '\"'
+		|| str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{'
+		|| str[i - 1] == '}')
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 'a' + 'A';
+			}
+		}
+		i++;
+	}
 
-    return (str);
+	return (str);
 }
