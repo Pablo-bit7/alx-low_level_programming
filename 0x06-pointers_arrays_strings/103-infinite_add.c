0651da5884af1a0d4927527d527a5e26affa5150
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-07-28 05:17:16 +0200

    betty round 1

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,63 +1,73 @@
-#include <stdio.h>
 #include "main.h"
 
+/**
+ * infinite_add - adds two numbers
+ *
+ * @n1: first number
+ *
+ * @n2: second number
+ *
+ * @r: buffer to store the result
+ *
+ * @size_r: buffer size
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int len1 = 0, len2 = 0, carry = 0, sum;
-    char *p1 = n1, *p2 = n2, *pr = r;
+	int len1 = 0, len2 = 0, carry = 0, sum;
+	char *p1 = n1, *p2 = n2, *pr = r;
 
-    /* Calculate lengths of n1 and n2 */
-    while (*p1)
-    {
-        len1++;
-        p1++;
-    }
-    while (*p2)
-    {
-        len2++;
-        p2++;
-    }
+	/* Calculate lengths of n1 and n2 */
+	while (*p1)
+	{
+		len1++;
+		p1++;
+	}
+	while (*p2)
+	{
+		len2++;
+		p2++;
+	}
 
-    /* Check if the result can fit in the buffer */
-    if (len1 > size_r - 1 || len2 > size_r - 1)
-    {
-        return (0);
-    }
+	/* Check if the result can fit in the buffer */
+	if (len1 > size_r - 1 || len2 > size_r - 1)
+	{
+		return (0);
+	}
 
-    /* Add digits from right to left */
-    p1 = n1 + len1 - 1;
-    p2 = n2 + len2 - 1;
-    pr = r + size_r - 1;
-    *pr = '\0'; /* Null-terminate the result string */
+	/* Add digits from right to left */
+	p1 = n1 + len1 - 1;
+	p2 = n2 + len2 - 1;
+	pr = r + size_r - 1;
+	*pr = '\0'; /* Null-terminate the result string */
 
-    while (p1 >= n1 || p2 >= n2)
-    {
-        sum = carry;
-        if (p1 >= n1)
-        {
-            sum += *p1 - '0';
-            p1--;
-        }
-        if (p2 >= n2)
-        {
-            sum += *p2 - '0';
-            p2--;
-        }
+	while (p1 >= n1 || p2 >= n2)
+	{
+		sum = carry;
+		if (p1 >= n1)
+		{
+			sum += *p1 - '0';
+			p1--;
+		}
+		if (p2 >= n2)
+		{
+			sum += *p2 - '0';
+			p2--;
+		}
 
-        carry = sum / 10;
-        *pr = (sum % 10) + '0';
-        pr--;
-    }
+		carry = sum / 10;
+		*pr = (sum % 10) + '0';
+		pr--;
+	}
 
-    /* Check if the result can fit in the buffer */
-    if (carry && pr >= r)
-    {
-        *pr = carry + '0';
-    }
-    else if (carry)
-    {
-        return (0);
-    }
+	/* Check if the result can fit in the buffer */
+	if (carry && pr >= r)
+	{
+		*pr = carry + '0';
+	}
+	else if (carry)
+	{
+		return (0);
+	}
 
-    return (pr + 1);
+	return (pr + 1);
 }
