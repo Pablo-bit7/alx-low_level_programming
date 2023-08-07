3162447a6a6eb8f5b1efab2ed24565152888de06
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-08-08 00:08:27 +0200

    Ran recompile.sh & updated libmy.a

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -8,5 +8,5 @@
 */
 int _isupper(int c)
 {
-	return (c >= 'A' && c <= 'Z') ? 1 : 0;
+	return ((c >= 'A' && c <= 'Z') ? 1 : 0);
 }
---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _atoi - copies a string
+*
+* @s: ...
+*
+* Return: ...
+*/
+int _atoi(char *s)
+{
+
+}
