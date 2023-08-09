c7ac3c8d300d5bcbaf100f15f8f805abd1f45754
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-08-09 04:53:55 +0200

    small edits first

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,7 +10,9 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	if (size == 0)
+	unsigned int i;
+
+    if (size == 0)
 		return (NULL);
 
 	char *array = (char *)malloc(size * sizeof(char));
@@ -18,7 +20,7 @@ char *create_array(unsigned int size, char c)
 	if (array == NULL)
 		return (NULL);
 
-	for (unsigned int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		array[i] = c;
 
 	return (array);
