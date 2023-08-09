1a097e6359e8480eb192d182ba569b0a22f323fe
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-08-09 03:32:08 +0200

    Brute force

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,6 +3,8 @@
 #include <stdbool.h>
 #include <ctype.h>
 
+#define UNUSED(x) (void)(x)
+
 /**
  * main - Entry point
  * @argc: The number of command-line arguments
@@ -12,5 +14,48 @@
  */
 int main(int argc, char *argv[])
 {
-	return (add_positive_numbers(argc, argv));
+	int i;
+	int j;
+
+	UNUSED(argv);    
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		int num = 0;
+		bool is_valid = true;
+
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				is_valid = false;
+				break;
+			}
+		}
+
+		if (is_valid)
+		{
+			num = atoi(argv[i]);
+			if (num > 0)
+			{
+				sum += num;
+			}
+		}
+		else
+		{
+		printf("Error\n");
+		return (1);
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
 }
