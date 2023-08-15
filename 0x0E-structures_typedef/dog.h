c5fc1d80f9dc34ad13d068b85ce0529b221cb95e
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-08-15 03:33:34 +0200

    betty dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,22 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+/**
+ * struct dog - Represents information about a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Description: Defines a type to store information about a dog,
+ * its (name, age, owner's name). The members of the structure
+ * allow for storing and accessing these attributes
+ * of a dog in a structured way.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif /* DOG_H */
