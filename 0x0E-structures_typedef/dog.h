7b16c2f241d39a3ceda4e7d54c6203813fd5e70b
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-08-15 04:35:14 +0200

    4-new_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,10 +19,9 @@ typedef struct dog
 	char *owner;
 } dog_t;
 
-/* Function prototype for init_dog */
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
 /* Function prototypes */
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* DOG_H */
