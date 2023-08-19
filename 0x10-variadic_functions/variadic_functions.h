d815680a609fef9baa60c4ea909c3f758ad5d9fb
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-08-19 04:33:45 +0200

    2-print_strings.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,12 +1,14 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdio.h>
 #include <stdarg.h>
-#include <stdio.h> /* Include for _putchar */
+#include <stddef.h>
 #include <stdlib.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
