c3375de03d4369e408e888c388487bec51b7c103
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-08-25 05:59:45 +0200

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+/* This function will be executed before main */
+void __attribute__((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
