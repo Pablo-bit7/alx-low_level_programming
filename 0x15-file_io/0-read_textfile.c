986d1bc05bc8b1a2aed57253de3e9bca08a63ad4
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-09-06 06:16:59 +0200

    1-create_file.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,40 +13,40 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, read_result, write_result;
-	char *buffer;
+    int fd, read_result, write_result;
+    char *buffer;
 
-	if (filename == NULL)
-		return (0);
+    if (filename == NULL)
+        return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		close(fd);
-		return (0);
-	}
+    buffer = malloc(sizeof(char) * letters);
+    if (buffer == NULL)
+    {
+        close(fd);
+        return (0);
+    }
 
-	read_result = read(fd, buffer, letters);
-	if (read_result == -1)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
+    read_result = read(fd, buffer, letters);
+    if (read_result == -1)
+    {
+        free(buffer);
+        close(fd);
+        return (0);
+    }
 
-	write_result = write(STDOUT_FILENO, buffer, read_result);
+    write_result = write(STDOUT_FILENO, buffer, read_result);
 
-	free(buffer);
-	close(fd);
+    free(buffer);
+    close(fd);
 
-	if (write_result == -1 || (size_t)write_result != (size_t)read_result)
-		return (0);
+    if (write_result == -1 || (size_t)write_result != (size_t)read_result)
+        return (0);
 
-	write(STDOUT_FILENO, "\n", 1);
+    write(STDOUT_FILENO, "\n", 1);
 
-	return (read_result);
+    return (read_result);
 }
