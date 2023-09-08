d4d0ad7b30eacf359bcf824973449d4fc20e973c
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-09-08 11:00:17 +0200

    0-read_textfile.c retry

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,40 +13,22 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd, read_result, write_result;
-    char *buffer;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-    if (filename == NULL)
-        return (0);
+	fd = open(filename, O_RDONLY);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	if (fd == -1)
+		return (0);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-    {
-        close(fd);
-        return (0);
-    }
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-    read_result = read(fd, buffer, letters);
-    if (read_result == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	free(buf);
+	close(fd);
 
-    write_result = write(STDOUT_FILENO, buffer, read_result);
-
-    free(buffer);
-    close(fd);
-
-    if (write_result == -1 || (size_t)write_result != (size_t)read_result)
-        return (0);
-
-    write(STDOUT_FILENO, "\n", 1);
-
-    return (read_result);
+	return (w);
 }
