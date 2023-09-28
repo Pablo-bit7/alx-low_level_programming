e1e3dc6acadb8606a325a3d14c57522863d4dfa7
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-09-29 00:45:00 +0200

    Edits (5-flip_bits.c)

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * flip_bits - Returns number of bits needed to flip to get from n to m.
- * @n: The source number.
- * @m: The destination number.
- *
- * Return: The number of bits needed to flip.
- */
+* flip_bits - Func returns number of bits needed to flip to get from n to m.
+* @n: The source number.
+* @m: The destination number.
+*
+* Return: The number of bits needed to flip.
+*/
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int xor_result = n ^ m;
