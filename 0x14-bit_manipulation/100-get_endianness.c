e30f0eb9de911b374d972f6c0c102d222767bece
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-09-29 00:52:21 +0200

    Edits (100-get_endianness.c)

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * get_endianness - Checks the endianness of the system.
- *
- * Return: 0 if big endian, 1 if little endian.
- */
+* get_endianness - Func checks the endianness of the system.
+*
+* Return: 0 if big endian, 1 if little endian.
+*/
 int get_endianness(void)
 {
 	unsigned int x = 1;
