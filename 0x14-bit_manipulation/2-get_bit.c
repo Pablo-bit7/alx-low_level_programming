39c05744b968660291860f22528a4801a540f132
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-11-24 04:52:02 +0200

    Betty my betty

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-* get_bit - Func returns the value of a bit at a given index.
-* @n: The number from which to retrieve the bit.
-* @index: The index of the bit to be retrieved.
-*
-* Return: Value of bit at the specified index or -1 if an error occurred.
-*/
+ * get_bit - Returns the value of a bit at a given index.
+ * @n: The number from which to retrieve the bit.
+ * @index: The index of the bit to be retrieved.
+ *
+ * Return: The value of the bit at the specified index or -1 if an error occurred.
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index >= sizeof(n) * 8)
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-* clear_bit - Func sets the value of a bit to 0 at a given index.
-* @n: A pointer to the number in which to clear the bit.
-* @index: The index of the bit to be cleared.
-*
-* Return: 1 if successful, or -1 if an error occurred.
-*/
+ * clear_bit - Sets the value of a bit to 0 at a given index.
+ * @n: A pointer to the number in which to clear the bit.
+ * @index: The index of the bit to be cleared.
+ *
+ * Return: 1 if successful, or -1 if an error occurred.
+ */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(*n) * 8)
