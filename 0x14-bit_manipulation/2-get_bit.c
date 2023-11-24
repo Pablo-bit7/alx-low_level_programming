228293e685f2c0bf7c6c0f70d1bcf52a33220d6e
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-11-24 05:44:59 +0200

    edits to `get_bit`

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,12 +5,20 @@
  * @n: The number from which to retrieve the bit.
  * @index: The index of the bit to be retrieved.
  *
- * Return: The value of the bit at the specified index or -1 if an error occurred.
+ * Return: Value of bit at the specified index or -1 if an error occurred.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= sizeof(n) * 8)
+	unsigned long int bitmask, bitValue;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
 
-	return ((n >> index) & 1);
+	bitmask = 1 << index;
+	bitValue = n & bitmask;
+
+	if (bitValue == bitmask)
+		return (1);
+
+	return (0);
 }
