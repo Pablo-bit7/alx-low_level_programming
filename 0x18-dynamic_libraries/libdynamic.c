97e5744197a2f54d8c392c14d57826294b1f4c16
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2024-01-16 04:40:02 +0200

    Getting there

--- 0x18-dynamic_libraries/libdynamic.c
+++ 0x18-dynamic_libraries/libdynamic.c
@@ -2,120 +2,268 @@
 
 int _putchar(char c)
 {
-	/* Placeholder implementation */
-	return (0);
+	return (write(1, &c, 1));
 }
 
 int _islower(int c)
 {
-	/* Placeholder implementation */
-	return (0);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
 
 int _isalpha(int c)
 {
-	/* Placeholder implementation */
-	return (0);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
 
 int _abs(int n)
 {
-	/* Placeholder implementation */
-	return (0);
+	return ((n >= 0) ? n : -n);
 }
 
 int _isupper(int c)
 {
-	/* Placeholder implementation */
-	return (0);
+	return ((c >= 'A' && c <= 'Z') ? 1 : 0);
 }
 
 int _isdigit(int c)
 {
-	/* Placeholder implementation */
-	return (0);
+	return ((c >= '0' && c <= '9') ? 1 : 0);
 }
 
 int _strlen(char *s)
 {
-	/* Placeholder implementation */
-	return (0);
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
 }
 
 void _puts(char *s)
 {
-	/* Placeholder implementation */
+	while (*s)
+	{
+		_putchar(*s);
+		str++;
+	}
+	_putchar('\n');
 }
 
 char *_strcpy(char *dest, char *src)
 {
-	/* Placeholder implementation */
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	dest[i] = '\0';
+
 	return (dest);
 }
 
 int _atoi(char *s)
 {
-	/* Placeholder implementation */
-	return (0);
+	int index, sign = 1, state = 0;
+	long value = 0;
+
+	for (index = 0; s[index] != '\0' && state != 2; index++)
+	{
+		if (s[index] == '-')
+			sign *= -1;
+
+		if (s[index] >= '0' && s[index] <= '9')
+		{
+			state = 1;
+			value = value * 10 + (s[index] - '0');
+
+			if (value > INT_MAX || value < INT_MIN)
+				exit(EXIT_FAILURE);
+		}
+		else if (state == 1)
+			state = 2;
+	}
+
+	return ((int)(sign * value));
 }
 
 char *_strcat(char *dest, char *src)
 {
-	/* Placeholder implementation */
+	char *ptr = dest;
+
+	while (*ptr)
+		ptr++;
+
+	while (*src)
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+	}
+	*ptr = '\0';
+
 	return (dest);
 }
 
 char *_strncat(char *dest, char *src, int n)
 {
-	/* Placeholder implementation */
+	char *ptr = dest;
+
+	while (*ptr)
+		ptr++;}
+
+	while (*src && n > 0)
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+		n--;
+	}
+	*ptr = '\0';
+
 	return (dest);
 }
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	/* Placeholder implementation */
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
 	return (dest);
 }
 
 int _strcmp(char *s1, char *s2)
 {
-	/* Placeholder implementation */
-	return (0);
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
 }
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	/* Placeholder implementation */
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
 	return (s);
 }
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	/* Placeholder implementation */
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
 	return (dest);
 }
 
 char *_strchr(char *s, char c)
 {
-	/* Placeholder implementation */
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	if (*s == c)
+		return (s);
+
 	return (NULL);
 }
 
 unsigned int _strspn(char *s, char *accept)
 {
-	/* Placeholder implementation */
-	return (0);
+	unsigned int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				break;
+			}
+		}
+
+		if (!accept[j])
+		{
+			break;
+		}
+	}
+
+	return (i);
 }
 
 char *_strpbrk(char *s, char *accept)
 {
-	/* Placeholder implementation */
+	unsigned int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+		}
+	}
+
 	return (NULL);
 }
 
 char *_strstr(char *haystack, char *needle)
 {
-	/* Placeholder implementation */
+	int i, j;
+
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+				{
+					break;
+				}
+			}
+			if (needle[j] == '\0')
+			{
+				return (haystack + i);
+			}
+		}
+	}
+
 	return (NULL);
 }
 
