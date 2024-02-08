0f45d35b22bf28396ddac24f1638521d3818c3a0
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2024-02-08 04:03:50 +0200

    0-hash_table_create.c

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -34,12 +34,7 @@ typedef struct hash_table_s {
 } hash_table_t;
 
 /* Function prototypes */
-shash_table_t *shash_table_create(unsigned long int size);
-int shash_table_set(shash_table_t *ht, const char *key, const char *value);
-char *shash_table_get(const shash_table_t *ht, const char *key);
-void shash_table_print(const shash_table_t *ht);
-void shash_table_print_rev(const shash_table_t *ht);
-void shash_table_delete(shash_table_t *ht);
+hash_table_t *hash_table_create(unsigned long int size);
 
 #endif /* HASH_TABLE_H */
 
